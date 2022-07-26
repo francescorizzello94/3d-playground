ab1ab7217f7844b4338c85645478feb61c6bab11
Francesco Pietro Rizzello <francescorizzello94@gmail.com>
2022-07-26 23:39:17 +0200

    restyle main page

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom';
 function App() {
   return (
     <div className="wrapper">
-      <h1 class="app-header">3D Playground</h1>
+      <h1 class="app-header">3D PLAYGROUND</h1>
       <p class="app-description">
         Pick a playground and forget
         about the outside world!
       </p>
-      <nav>
-        <Link to="/Geometry">Geometry</Link> | { " "}
+      <nav class="app-nav">
+        <Link to="/Geometry">Geometry</Link> | { ""}
         <Link to="/VisualEffects">Visual Effects</Link>
       </nav>
       </div>
