8ae50f249a4cb014357c863a1d5346812d80d8e0
Francesco Pietro Rizzello <francescorizzello94@gmail.com>
2022-07-26 12:53:20 +0200

    revert to initial geometry page

--- src/Pages/Geometry.js
+++ src/Pages/Geometry.js
@@ -2,13 +2,6 @@ import { Canvas } from "@react-three/fiber"
 
 export const Geometry = () => { 
   return (
-    <Canvas>
-    {/* <h1>Geometry</h1> */}
-    <pointLight position={[10, 10, 10]} />
-    <mesh>
-      <sphereBufferGeometry />
-      <meshStandardMaterial color="hotpink" />
-    </mesh>
-  </Canvas>
+    <h1>Geometry</h1>
   )
 }
