cdd96d781f801804adc860dbf3f46d0386f46657
Francesco Pietro Rizzello <francescorizzello94@gmail.com>
2022-07-26 18:03:26 +0200

    add link to homepage in visual effects page, update geometry visual effects css

--- src/Pages/VisualEffects.js
+++ src/Pages/VisualEffects.js
@@ -1,5 +1,19 @@
+import { Canvas } from "@react-three/fiber"
+import App from "../App"
+import { Link } from "react-router-dom"
+import "./VisualEffects.css"
+
 export const VisualEffects = () => {
   return (
-    <h1>Visual Effects</h1>
+    <div>
+      <h1>Visual Effects</h1>
+      <div id="description">
+        Let the visions take you far away
+        <br />
+        <nav id="return">
+          <Link to="/"> HomePage</Link>
+        </nav>
+      </div>
+    </div>
   )
 }
