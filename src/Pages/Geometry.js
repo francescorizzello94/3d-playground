090717b1d1a8c9667cae60f19382b5b21b71dcf6
Francesco Pietro Rizzello <francescorizzello94@gmail.com>
2022-07-27 12:36:27 +0200

    import fontawesome package

--- src/Pages/Geometry.js
+++ src/Pages/Geometry.js
@@ -2,17 +2,22 @@ import './Geometry.css'
 import { Canvas } from "@react-three/fiber"
 import App from "../App"
 import { Link } from "react-router-dom"
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faArrowCircleRight} from '@fortawesome/free-solid-svg-icons/faArrowCircleRight'
 
 export const Geometry = () => { 
   return (
-    <div class="wrapper-geometry">
-      <h1 class="geometry-header">Geometry</h1>
-      <div class="description-geometry">
+    <div className="wrapper-geometry">
+      <h1 className="geometry-header">Geometry</h1>
+      <div className="description-geometry">
         Explore the realm of geometry
         <br />
-        <nav class="return-geometry">
+        <nav className="return-geometry">
           <Link to="/"> HomePage</Link>
+        </nav>
+        <nav className="geometry-menu">
+          <a href=".cube"><FontAwesomeIcon icon={faArrowCircleRight } /></a>
+          <a href=".sphere"><FontAwesomeIcon icon={faArrowCircleRight} /></a>
         </nav>
       </div>
     </div>
