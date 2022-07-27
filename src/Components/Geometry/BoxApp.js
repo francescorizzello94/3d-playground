63271d2e676f2623079f27388c972ee6b9a075e8
Francesco Pietro Rizzello <francescorizzello94@gmail.com>
2022-07-28 00:39:13 +0200

    modify file structure initialize components folder

---
+++ src/Components/Geometry/BoxApp.js
@@ -0,0 +1,44 @@
+import * as THREE from 'three'
+import { useRef, useEffect, useState } from 'react'
+import { Canvas, useFrame } from "@react-three/fiber"
+import { OrbitControls } from '@react-three/drei'
+import * as dat from 'dat.gui'
+
+function Box(props) {
+
+  const mesh = useRef();
+
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? [2, 1.5, 1.5] : [1, 1, 1]}
+      onClick={e => setActive(!active)}
+      onPointerOver={e => setHover(true)}
+      onPointerOut={e => setHover(false)}>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshStandardMaterial
+        attach="material"
+        color={hovered ? 'hotpink' : 'orange'}
+      />
+    </mesh>
+  );
+}
+
+export default function BoxApp() {
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+
+  );
+}
--- src/Pages/Geometry.js
+++ src/Pages/Geometry.js
@@ -1,10 +1,6 @@
 import './Geometry.css'
-import * as THREE from 'three'
-import { useRef, useState } from 'react'
-import { Canvas, useFrame } from "@react-three/fiber"
-import { OrbitControls } from '@react-three/drei'
+import BoxApp from '../Components/Geometry/BoxApp'
 import { Link } from "react-router-dom"
-import * as dat from 'dat.gui'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowCircleRight } from '@fortawesome/free-solid-svg-icons/faArrowCircleRight'
 
@@ -35,41 +31,3 @@ export const Geometry = () => {
   )
 }
 
-function Box(props) {
-  // This reference will give us direct access to the mesh
-  const mesh = useRef();
-
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
-
-  // Rotate mesh every frame, this is outside of React without overhead
-  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
-
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-      onClick={e => setActive(!active)}
-      onPointerOver={e => setHover(true)}
-      onPointerOut={e => setHover(false)}>
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshStandardMaterial
-        attach="material"
-        color={hovered ? 'hotpink' : 'orange'}
-      />
-    </mesh>
-  );
-}
-
-export default function BoxApp() {
-  return (
-      <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
-      </Canvas>
-  );
-}
