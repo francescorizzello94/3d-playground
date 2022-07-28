34caa3ee2b9e8641c69ae66644114460a5458256
Francesco Pietro Rizzello <francescorizzello94@gmail.com>
2022-07-28 02:52:27 +0200

    add three gui cubes gui control not showing on drop down

---
+++ src/Components/Geometry/InteractiveRect.js
@@ -0,0 +1,55 @@
+import { useRef, useState } from 'react';
+import { useFrame } from 'react-three-fiber';
+import { Canvas } from 'react-three-fiber';
+import { Controls, useControl } from 'react-three-gui';
+
+function ThreeInteractiveBoxes({ speed, ...props }) {
+  const mesh = useRef();
+
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(true);
+
+  useFrame(
+    () =>
+      (mesh.current.rotation.x = mesh.current.rotation.y += active ? speed : 0),
+  );
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={[1, 1, 1]}
+      onClick={(e) => setActive(!active)}
+      onPointerOver={(e) => setHover(true)}
+      onPointerOut={(e) => setHover(false)}
+    >
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshStandardMaterial
+        attach="material"
+        color={hovered ? 'tomato' : 'steelblue'}
+      />
+    </mesh>
+  );
+}
+
+export default function ThreeCubesApp() {
+  const { animation: { to: rotateSpeed = 0 } = {} } = useControl('Rotate Speed', {
+    type: 'number',
+    min: -0.1,
+    max: 0.1,
+    spring: true,
+  });
+
+  return (
+    <>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <ThreeInteractiveBoxes position={[-2, 0, 0]} speed={rotateSpeed} />
+        <ThreeInteractiveBoxes position={[0, 0, 0]} speed={rotateSpeed} />
+        <ThreeInteractiveBoxes position={[2, 0, 0]} speed={rotateSpeed} />
+      </Canvas>
+      <Controls />
+    </>
+  );
+}
--- src/Pages/Geometry.js
+++ src/Pages/Geometry.js
@@ -1,6 +1,7 @@
 import './Geometry.css'
 import BoxApp from '../Components/Geometry/BoxApp'
 import NintendoCube from '../Components/Geometry/NintendoCube'
+import ThreeCubesApp from '../Components/Geometry/InteractiveRect'
 import { Link } from "react-router-dom"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowCircleRight } from '@fortawesome/free-solid-svg-icons/faArrowCircleRight'
@@ -13,13 +14,13 @@ export const Geometry = () => {
         Explore the realm of geometry
         <br />
         <nav className="return-geometry">
-          
+
         </nav>
         <nav className="geometry-menu">
           <Link to="/">Home</Link>
           <a href="#geo-cube-icon"><FontAwesomeIcon icon={faArrowCircleRight} /></a>
           <a href="#geo-twocube-icon"><FontAwesomeIcon icon={faArrowCircleRight} /></a>
-          <a href="#geo-controlcube-icon"><FontAwesomeIcon icon={faArrowCircleRight} /></a>
+          <a href="#geo-controlrect-icon"><FontAwesomeIcon icon={faArrowCircleRight} /></a>
         </nav>
         <div className="geometry-icons-wrapper">
           <section id="geo-cube-icon">
@@ -28,8 +29,8 @@ export const Geometry = () => {
           <section id="geo-twocube-icon">
             <BoxApp />
           </section>
-          <section id="geo-controlcube-icon">
-
+          <section id="geo-controlrect-icon">
+            <ThreeCubesApp />
           </section>
         </div>
       </div>
