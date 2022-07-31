631e90b96e1954b8fcb5032d24c2e6714b6d69fa
Francesco Pietro Rizzello <francescorizzello94@gmail.com>
2022-07-31 14:12:08 +0200

    add transforming intersection of multiple rectangles in geometry

--- src/Components/Geometry/InteractiveRect.js
+++ src/Components/Geometry/InteractiveRect.js
@@ -1,55 +1,105 @@
-import { useRef, useState, useMemo } from 'react';
-import { useFrame } from 'react-three-fiber';
-import { Canvas } from 'react-three-fiber';
-import { Controls, useControl } from 'react-three-gui';
+import React, { useRef, useEffect } from 'react'
+import { Canvas, useFrame } from 'react-three-fiber'
+import create from 'zustand'
+import { useControl } from 'react-three-gui'
 
-function ThreeInteractiveBoxes({ speed, ...props }) {
-  const mesh = useRef();
+const rpi = () => Math.random() * Math.PI
+const [useStore, api] = create(set => ({
+  amount: 0,
+  boxes: [],
+  coords: [],
+  create(amount) {
+    const ids = new Array(parseInt(amount)).fill().map((_, i) => i)
+    set({
+      amount,
+      boxes: ids,
+      coords: ids.reduce((acc, id) => ({ ...acc, [id]: [rpi(), rpi(), rpi()] }), 0)
+    })
+  },
+  advance(state) {
+    set(state => {
+      const coords = {}
+      for (let i = 0; i < state.boxes.length; i++) {
+        const id = state.boxes[i]
+        const [x, y, z] = state.coords[id]
+        coords[id] = [x + 0.01, y + 0.01, z + 0.01]
+      }
+      return { ...state, coords }
+    })
+  }
+}))
 
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(true);
-
-  useFrame(
-    () =>
-      (mesh.current.rotation.x = mesh.current.rotation.y += active ? speed : 0),
-  );
-
+function ItemSlow({ id }) {
+  const coords = useStore(state => state.coords[id])
+  if (!coords) return null
   return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={[1, 1, 1]}
-      onClick={(e) => setActive(!active)}
-      onPointerOver={(e) => setHover(true)}
-      onPointerOut={(e) => setHover(false)}
-    >
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshStandardMaterial
-        attach="material"
-        color={hovered ? 'tomato' : 'steelblue'}
-      />
+    <mesh rotation={coords}>
+      <boxBufferGeometry args={[2, 2, 2]} attach="geometry" />
+      <meshNormalMaterial attach="material" />
     </mesh>
-  );
+  )
 }
 
-export default function ThreeCubesApp() {
-  const { animation: { to: rotateSpeed = 0 } = {} } = useControl('Rotate Speed', {
-    type: 'number',
-    min: -0.1,
-    max: 0.1,
-    spring: true,
-  });
+function ItemFast({ id }) {
+  const mesh = useRef()
+  const coords = useRef([0, 0, 0])
+  useEffect(() => api.subscribe(xyz => (coords.current = xyz), state => state.coords[id]))
+  useFrame(() => mesh.current && mesh.current.rotation.set(...coords.current))
+  return (
+    <mesh ref={mesh}>
+      <boxBufferGeometry args={[2, 2, 2]} attach="geometry" />
+      <meshNormalMaterial attach="material" />
+    </mesh>
+  )
+}
+
+function ThreeInteractiveBoxes() {
+  const ref = useRef()
+  const boxes = useStore(state => state.boxes)
+  const amount = useControl('Amount️', { type: 'select', items: [20, 100, 200, 500, 1000, 2000] })
+  const root = useControl('React', { type: 'select', group: 'performance', items: ['legacy (slow)', 'concurrent (fast)'] })
+  const flux = useControl('Zustand', { type: 'select', group: 'performance', items: ['reactive (slow)', 'transient (fast)'] })
+  const concurrent = root === 'concurrent (fast)'
+  const transient = flux === 'transient (fast)'
+  const Component = transient ? ItemFast : ItemSlow
+
+  useEffect(() => {
+    let frame = undefined
+    api.getState().create(amount)
+    let lastCalledTime = Date.now()
+    let fps = 0
+    function renderLoop() {
+      let delta = (Date.now() - lastCalledTime) / 1000
+      lastCalledTime = Date.now()
+      fps = 1 / delta
+      // Change state every frame
+      api.getState().advance()
+      frame = requestAnimationFrame(renderLoop)
+    }
+    renderLoop()
+    return () => cancelAnimationFrame(frame)
+  }, [amount, concurrent, transient])
 
   return (
-    <>
-        <Canvas>
-          <ambientLight />
-          <pointLight position={[10, 10, 10]} />
-          <ThreeInteractiveBoxes position={[-2, 0, 0]} speed={rotateSpeed} />
-          <ThreeInteractiveBoxes position={[0, 0, 0]} speed={rotateSpeed} />
-          <ThreeInteractiveBoxes position={[2, 0, 0]} speed={rotateSpeed} />
-        </Canvas>
-        <Controls />
-    </>
-  );
+    <div class="main" style={{ background: transient || concurrent ? '#272737' : 'transparent' }}>
+      <Canvas >
+        <ErrorBoundaries>
+          {boxes.map(id => (
+            <Component key={id} id={id} />
+          ))}
+        </ErrorBoundaries>
+      </Canvas>
+      <div ref={ref} class="fps" />
+    </div>
+  )
 }
+
+class ErrorBoundaries extends React.Component {
+  state = { hasError: false }
+  static getDerivedStateFromError = error => ({ hasError: true })
+  render = () => (this.state.hasError ? null : this.props.children)
+}
+
+
+
+export default ThreeInteractiveBoxes;
